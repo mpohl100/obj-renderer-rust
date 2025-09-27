be90519b6d55836e6265e31cfcb01e9992e6ab17
Michael Pohl <mpohl100@googlemail.com>
2025-09-27 16:10:13 +0200

    remove dir mistake

--- .devcontainer/.devcontainer/develop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Build the Docker image
-docker build -t develop-image $(pwd)/.devcontainer/.
-
-# Run the Docker container interactively with the /project directory mounted
-docker run -it -v $(pwd):/workspace -e EXECUTE_BIN_BASH=1 develop-image
